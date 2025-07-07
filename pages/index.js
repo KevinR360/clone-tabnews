2359e4fb5c5b5e5c15a40255b458d5d7b93d1618
KevinR360 <kevin.rs.201057@gmail.com>
2025-07-07 02:22:03 -0300

    first-step

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Clone Tab News</h1>
+      <p>This is a simple Next.js application.</p>
+    </div>
+  );
+}
+
+export default Home;
