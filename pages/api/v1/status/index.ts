448da6b3f38cb62ae2082c1128861ae592ca6c90
KevinR360 <kevin.rs.201057@gmail.com>
2025-09-18 00:09:23 -0300

    add  and

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,9 +1,12 @@
 //@ts-check
 
 import { NextApiRequest, NextApiResponse } from "next";
+import database from "../../../../infra/database.js";
 
-function status(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: 'mêsVersário' });
+async function status(req: NextApiRequest, res: NextApiResponse) {
+  const result = await database.query('SELECT 1 + 1 AS sum;');
+  console.log(result.rows);
+  res.status(200).json({ message: "mêsVersário" });
 }
 
 export default status;
