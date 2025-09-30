b80c9bb21a0a9967e7df1afb0455feece5fb6c49
KevinR360 <kevin.rs.201057@gmail.com>
2025-09-29 23:42:43 -0300

    add root path in the project

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,10 +1,10 @@
 //@ts-check
 
 import { NextApiRequest, NextApiResponse } from "next";
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(req: NextApiRequest, res: NextApiResponse) {
-  const result = await database.query('SELECT 1 + 1 AS sum;');
+  const result = await database.query("SELECT 1 + 1 AS sum;");
   console.log(result.rows);
   res.status(200).json({ message: "mêsVersário" });
 }
